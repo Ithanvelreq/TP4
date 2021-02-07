3ec6aa7de04d0fa5bc6656db4e3b711f0be488a0
tmdr21 <t.mdarhri@gmail.com>
2021-02-07 19:08:37 +0100

    commentaires

--- Decoder.cpp
+++ Decoder.cpp
@@ -6,11 +6,11 @@
     e-mail               : ithan.velarde-requena@insa-lyon.fr taha.mdarhri@insa-lyon.fr
 *************************************************************************/
 
-//---------- R�alisation de la classe <Decoder> (fichier Decoder.cpp) ------------
+//---------- Realisation de la classe <Decoder> (fichier Decoder.cpp) ------------
 
 //---------------------------------------------------------------- INCLUDE
 
-//-------------------------------------------------------- Include syst�me
+//-------------------------------------------------------- Include systeme
 #include <iostream>
 #include <cstring>
 #include <fstream>
@@ -23,19 +23,19 @@ using namespace std;
 //------------------------------------------------------------- Constantes
 #define TAILLE 400
 //----------------------------------------------------------------- PUBLIC
-//----------------------------------------------------- M�thodes publiques
+//----------------------------------------------------- Methodes publiques
 void Decoder::Decode(ifstream & log, int time, int ext){
-int i=0;
+	int i=0;
 	string mot ;
-	while(log.peek() != EOF){
-		getline(log,mot);
+	while(log.peek() != EOF){  //on s'arrete si on arrive a la fin du fichier
+		getline(log,mot); //selectionne toute une ligne pour en extraire les infos
 		Data d;
 		d.ipadd = extract(mot,' ');
 		d.userlogname = extract(mot,' ');
 		d.A_user = extract(mot,' ');
 		extract(mot,'[');
 		d.date = extract(mot,':');
-		try{
+		try{  //Lorsqu'un attribut de type int n'est pas renseigné on le met à -1
 			d.heure = stoi(extract(mot,':'));
 		}catch (const invalid_argument & e){
 			d.heure = -1;
@@ -43,7 +43,7 @@ int i=0;
 		extract(mot,'"');
 		extract(mot,' ');
 		d.target = extract(mot,' ');
-		clean(d.target);
+		clean(d.target); //Nettoie le lien
 		extract(mot,' ');
 		try{
 			d.status = stoi(extract(mot,' '));
@@ -59,7 +59,8 @@ int i=0;
 		d.referer = extract(mot,'"');
 		clean(d.referer);
 		extract(mot,'"');
-		d.clientID = extract(mot,'"');		
+		d.clientID = extract(mot,'"');	
+		//Push_back si les conditions sont vérifiées (dépend du choix de l'utilisateur)	:
 		if ( ((ext && !extension(d.target)) || !ext) && ( (time<0 || time>23) || ( (time>=0 && time<=23) && d.heure==time) ) ){
 		infos.push_back(d);
 		}
@@ -85,6 +86,8 @@ Decoder::~Decoder()
 	#endif
 } //-----Fin de ~Decoder
 //------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Methodes protegees
 string Decoder::extract(string & mot, char delim){
 	int i = mot.find(delim);
 	string extrait = mot.substr(0,i);
@@ -93,7 +96,6 @@ string Decoder::extract(string & mot, char delim){
 }
 
 void Decoder::clean(string & url){
-//if(!strcmp("http://intranet-if.insa-lyon.fr",url.substr(0,31))){
 if(!url.substr(0,31).compare("http://intranet-if.insa-lyon.fr")){
 url =url.substr(31);
 }
@@ -113,7 +115,7 @@ return 0;
 }
 }
 
-//----------------------------------------------------- M�thodes prot�g�es
+
 
 
 
--- Decoder.h
+++ Decoder.h
@@ -1,7 +1,7 @@
 /*************************************************************************
                            Decoder  -  description
                              -------------------
-    début                : 20/01/2021
+    debut                : 20/01/2021
     copyright            : (C) 2021 par VELARDE REQUENA MDARHRI
     e-mail               : ithan.velarde-requena@insa-lyon.fr taha.mdarhri@insa-lyon.fr
 *************************************************************************/
@@ -9,7 +9,7 @@
 //---------- Interface de la classe <Decoder> (fichier Decoder.h) ----------------
 #if ! defined ( DECODER_H )
 #define DECODER_H
-//--------------------------------------------------- Interfaces utilisées
+//--------------------------------------------------- Interfaces utilisees
 #include <vector>
 #include <string>
 #include <iostream>
@@ -31,9 +31,9 @@ struct Data{
     Data(string ip="",string u="",string a="",string d="",int h =0, string t="",int st=0,int si=0,string r="",string c="") : ipadd(ip),userlogname(u), A_user(a),date(d),heure(h),target(t),status(st),size(si),referer(r),clientID(c) {}
 };
 //------------------------------------------------------------------------
-// Rôle de la classe <Decode>
+// Role de la classe <Decode>
 //Classe qui permet de lire le fichier log et en extraire les informations.
-//Ces dernières sont mises dans une structure Data, qui est elle-même insérée
+//Ces dernieres sont mises dans une structure Data, qui est elle-meme inseree
 //dans un vecteur de Data
 //------------------------------------------------------------------------
 
@@ -42,9 +42,9 @@ class Decoder
 //----------------------------------------------------------------- PUBLIC
 
 public:
-//----------------------------------------------------- Méthodes publiques
+//----------------------------------------------------- Mï¿½thodes publiques
 
-// type Méthode ( liste des paramètres );
+// type Methode ( liste des parametres );
 	void Decode(ifstream & log, int time=30, int ext=0);
 	//Extrait les informations
 
@@ -58,19 +58,20 @@ public:
 
 //----------------------------------------------------- Attributs publics
 	vector <Data> infos;
-	//Vecteur composée des informations extraites du fichier log
+	//Vecteur composee des informations extraites du fichier log
 
 
 //------------------------------------------------------------------ PRIVE
 protected:
-//----------------------------------------------------- Méthodes protégées
-string extract(string & mot, char delim);
-void clean(string & url);
-int extension(string target);
-//----------------------------------------------------- Attributs protégés
+//----------------------------------------------------- Methodes protegees
+string extract(string & mot, char delim); //extrait une partie de la ligne
+void clean(string & url); //Enleve ce qui pollue dans un lien
+int extension(string target); //Enleve les extensions image, css, js si l'on veut
+
+//----------------------------------------------------- Attributs proteges
 
 };
 
-//-------------------------------- Autres définitions dépendantes de <DECODER>
+//-------------------------------- Autres definitions dependantes de <DECODER>
 
 #endif // DECODER_H
